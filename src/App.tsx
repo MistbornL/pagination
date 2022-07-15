39a3c955d63d27c37ec59b947ea7c2b27d5d22fc
MistbornL <lashaloria3@gmail.com>
2022-07-15 17:38:31 +0400

    paginate

--- src/App.tsx
+++ src/App.tsx
@@ -2,31 +2,26 @@ import React, { useState } from "react";
 import "./App.css";
 import ReactPaginate from "react-paginate";
 
-function App() {
-  const items = [];
-  for (let i = 1; i < 1000; i++) {
-    items.push(i);
-  }
-
+const PaginateItems = ({ items }: any) => {
   const [pageNum, setPageNum] = useState<number>(0);
-  const itemsPerPage = 10;
-  const pagesVisited = pageNum * itemsPerPage;
+  const itemsPerPage: number = 10;
+  const pagesVisited: number = pageNum * itemsPerPage;
 
   const displayUsers = items
     .slice(pagesVisited, pagesVisited + itemsPerPage)
-    .map((item, index) => {
+    .map((item: any, index: number) => {
       return <p key={index}>item{item}</p>;
     });
 
-  const pageCount = Math.ceil(items.length / itemsPerPage);
+  const pageCount: number = Math.ceil(items.length / itemsPerPage);
 
   const onPageChange = (e: any) => {
     setPageNum(e.selected);
   };
+
   return (
-    <div className="App">
+    <>
       <div className="items" style={{ height: "360px" }}>
-        {" "}
         {displayUsers}
       </div>
       <div className="page">
@@ -40,6 +35,19 @@ function App() {
           nextLinkClassName={"nextBttn"}
         ></ReactPaginate>
       </div>
+    </>
+  );
+};
+
+function App() {
+  const items = [];
+  for (let i = 1; i < 1000; i++) {
+    items.push(i);
+  }
+
+  return (
+    <div className="App">
+      <PaginateItems items={items} />
     </div>
   );
 }
